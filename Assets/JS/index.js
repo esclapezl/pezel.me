54b2cff393ea21a888ea5ec6c823e0f84d6c13a1
Esclapez Loïc <loicesclapez@gmail.com>
2023-02-16 01:45:00 +0100

    texte intro et animation lettres

---
+++ Assets/JS/index.js
@@ -0,0 +1,26 @@
+/*
+    anime les lettres
+*/
+function animateLetter(){
+    const idLettres= [0, 1, 2, 3, 4];
+    const idLettresRandom = idLettres.sort((a, b) => 0.5 - Math.random());
+    
+    const lettres = document.getElementById('lettres');
+    let i = 0;
+    for (const n of idLettresRandom) {
+        lettres.children[n].style.animationDelay = (i*0.15+0.1)+'s';
+        i++;
+    }
+}
+
+animateLetter();
+
+function animateIntroTexte(){
+    const textes = document.getElementsByClassName('txtIntro');
+    textes[0].style.animationDelay = '1s';
+    textes[1].style.animationDelay = '1.8s';
+    textes[2].style.animationDelay = '2.1s';
+}
+
+animateIntroTexte();
+
