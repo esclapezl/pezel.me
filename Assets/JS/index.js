ed7da6430f747f4158e7fb63a931ce0806cb90d0
Esclapez Loïc <loicesclapez@gmail.com>
2023-02-23 01:01:38 +0100

    desgin progress

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -31,5 +31,36 @@ function animateIntroTexte(){
     }
 }
 
+
+function randomLetterAnim()
+{
+    let interval = null;
+    document.querySelector("b").onmouseover = event => {
+        clearInterval(interval);
+        let iteration = 0;
+       
+        const letters = "abcdefghijklmnopqrstuvwxyz"
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+          .split("")
+          .map((letter, index) => {
+            if(index < iteration) {
+              return event.target.dataset.value[index];
+            }
+          
+            return letters[Math.floor(Math.random() * 26)]
+          })
+          .join("");
+        
+        if(iteration >= event.target.dataset.value.length){ 
+          clearInterval(interval);
+        }
+        
+        iteration += 1 / 3;
+      }, 30);
+    };
+}
+
+
 animateIntroTexte();
 
